4ac00c4b3fab415c36512fec34fbf30d91e7f198
diego kogan <diegokogan@gmail.com>
2017-04-08 19:04:35 -0300

    playing with anko

--- app/src/main/java/com/diegok/expenses/MainActivity.kt
+++ app/src/main/java/com/diegok/expenses/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.diegok.expenses
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.text.InputType
+import android.widget.EditText
+import org.jetbrains.anko.*
+import pub.devrel.easypermissions.EasyPermissions
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), EasyPermissions.PermissionCallbacks {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        verticalLayout {
+            padding = dip(30)
+            var expense: EditText? = null
+            arrayListOf(
+                    textView {
+                        text = "Carrefour"
+                        backgroundColor = Color.BLUE
+                        onClick {
+                            expense?.setText(text)
+                            toast("clicked")
+                        }
+                    }.lparams(width = wrapContent),
+                    textView {
+                        text = "Carniceria"
+                        backgroundColor = Color.RED
+                        onClick {
+                            expense?.setText(text)
+                            toast("clicked")
+                        }
+                    }.lparams(width = wrapContent)
+            )
+
+            expense = editText {
+                hint = "Expense"
+                textSize = 24f
+            }
+            editText {
+                hint = "Cost"
+                inputType = InputType.TYPE_CLASS_NUMBER
+                textSize = 24f
+            }
+            button("Send") {
+                textSize = 26f
+                onClick {
+                    toast("hi")
+                }
+            }
+        }
+    }
+
+    override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>?) {
+        // Do nothing.
+    }
+
+    override fun onPermissionsDenied(requestCode: Int, perms: MutableList<String>?) {
+        TODO("not implemented")
     }
 }
